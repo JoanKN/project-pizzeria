d6124dc4aff6e5ca1b8b45a5a5158030fa369149
Joanna Koga-Naghmouchi <joanna_koga@tlen.pl>
2025-05-11 13:16:44 +0200

    Add basket

--- dist/js/script.js
+++ dist/js/script.js
@@ -277,6 +277,30 @@
       thisWidget.element.dispatchEvent(event);
     } 
   }
+  class Cart{
+    constructor(element){
+      const thisCart = this;
+      thisCart.products =[];
+      thisCart.getElements(element);
+      thisCart.initActions();
+      console.log('new Cart', thisCart);
+    }
+    getElements(element){
+      const thisCart = this;
+      thisCart.dom = {};
+      thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+    initActions() {
+    const thisCart = this;
+
+    if (thisCart.dom.toggleTrigger) {
+      thisCart.dom.toggleTrigger.addEventListener('click', function () {
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+    }
+  }
+  }
   const app = {
     initMenu: function () {
       const thisApp = this;
@@ -291,6 +315,11 @@
       const thisApp = this;
       thisApp.data = dataSource;
     },
+    initCart: function(){
+      const thisApp = this;
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
     init: function () {
       const thisApp = this;
       console.log('*** App starting ***');
@@ -300,6 +329,7 @@
       console.log('templates:', templates);
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
   };
   app.init();
